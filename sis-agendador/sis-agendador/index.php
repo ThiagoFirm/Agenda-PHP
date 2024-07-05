e443f8a64df656371927c1779633ff7b8f6008f4
Thiago Firmino <161902186+ThiagoFirm@users.noreply.github.com>
2024-07-04 23:56:39 -0300

    Add files via upload

--- sis-agendador/sis-agendador/index.php
+++ sis-agendador/sis-agendador/index.php
@@ -8,63 +8,86 @@ include("db/conexao.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/estilo-padrao.css">
+
     <title>Sistema Agendador</title>
 </head>
 
 <body>
-    <header>
-        <h1>Sistema Agendador</h1>
-        <nav>
-            <a href="index.php?menuop=home">Home</a>
-            <a href="index.php?menuop=contatos">Contatos</a>
-            <a href="index.php?menuop=eventos">Eventos</a>
-            <a href="index.php?menuop=tarefas">Tarefas</a>
-        </nav>
+    <header class="bg-dark">
+        <div class="container">
+            <h1>Sistema Agendador</h1>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a href="index.php?menuop=home">Home</a>
+                <a href="index.php?menuop=contatos">Contatos</a>
+                <a href="index.php?menuop=eventos">Eventos</a>
+                <a href="index.php?menuop=tarefas">Tarefas</a>
+            </nav>
+        </div>
     </header>
     <main>
         <?php
-        
-            $menuop = (isset($_GET['menuop']))?$_GET["menuop"]:"home";
-            switch ($menuop) {
-                case 'home':
-                    include("paginas/home/home.php");
-                    break;
-                
-                case 'contatos':
-                    include("paginas/contatos/contatos.php");
-                    break;
 
-                case 'cad-contato':
-                    include("paginas/contatos/cad-contato.php");
-                    break;
-
-                case 'inserir-contato':
-                    include("paginas/contatos/inserir-contato.php");
-                    break;
-
-                case 'editar-contato':
-                    include("paginas/contatos/editar-contato.php");
-                    break;
-
-                case 'atualizar-contato':
-                    include("paginas/contatos/atualizar-contato.php");
-                    break;
-
-                case 'tarefas':
-                    include("paginas/tarefas/tarefas.php");
-                    break;
-
-                case 'eventos':
-                    include("paginas/eventos/eventos.php");
-                    break;
-
-                default:
+        $menuop = (isset($_GET['menuop'])) ? $_GET["menuop"] : "home";
+        switch ($menuop) {
+            case 'home':
                 include("paginas/home/home.php");
                 break;
 
-            }
+            case 'contatos':
+                include("paginas/contatos/contatos.php");
+                break;
+
+            case 'cad-contato':
+                include("paginas/contatos/cad-contato.php");
+                break;
+
+            case 'inserir-contato':
+                include("paginas/contatos/inserir-contato.php");
+                break;
+
+            case 'editar-contato':
+                include("paginas/contatos/editar-contato.php");
+                break;
+
+            case 'atualizar-contato':
+                include("paginas/contatos/atualizar-contato.php");
+                break;
+
+            case 'excluir-contato':
+                include("paginas/contatos/excluir-contato.php");
+                break;
+
+            case 'tarefas':
+                include("paginas/tarefas/tarefas.php");
+                break;
+
+            case 'eventos':
+                include("paginas/eventos/eventos.php");
+                break;
+
+            default:
+                include("paginas/home/home.php");
+                break;
+        }
         ?>
     </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+
 </body>
 
+
+<style>
+
+body{
+    background-color: #3b4147;
+    color: white;
+}
+
+</style>
 </html>
